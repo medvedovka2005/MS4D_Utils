facb0066c4035c4dfe2a501c8e3eaab3cd1798ba
Роман Лысенко <medvedovka@gmail.com>
2024-07-11 13:34:14 +0300

    Передвинул кнопку

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1911,7 +1911,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(878, 25);
+            this.button3.Location = new System.Drawing.Point(980, 25);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 43;
